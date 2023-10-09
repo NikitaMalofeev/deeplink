a183fcd5e95ff910aec7264f70beab989d98f83b
NikitaM <front.work3@gmail.com>
2023-10-09 18:38:43 +0600

    add deeplink

---
+++ android/app/src/main/java/ionic/deeplink/MainActivity.java
@@ -0,0 +1,5 @@
+package ionic.deeplink;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <ion-app>
+    <ion-router-outlet />
+  </ion-app>
+</template>
+
+<script lang="ts">
+import { IonApp, IonRouterOutlet } from '@ionic/vue';
+import { App, URLOpenListenerEvent } from '@capacitor/app';
+import { defineComponent } from 'vue';
+import { useRouter } from "vue-router";
+
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    IonApp,
+    IonRouterOutlet
+  },
+  setup() {
+
+  const router = useRouter();
+
+
+  App.addListener('appUrlOpen', function (event: URLOpenListenerEvent) {
+ 
+  const slug = event.url.split('.app').pop();
+
+  // We only push to the route if there is a slug present
+  if (slug) {
+    router.push({
+      path: slug,
+    });
+  }
+  
+});
+
+  } 
+  })
+</script>
